07601c297cbd8d6450c3509d4ef45123d84d3cef
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-08-11 13:55:33 -0700

    edit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program tha prints with ptintf function
+ *
+ * Retur: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
