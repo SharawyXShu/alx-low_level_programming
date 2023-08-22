a66b10cff9681e7c7af5ab8a312094028203cdd4
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-08-22 12:21:00 -0700

    uu

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts -prints a string, followed by a newline to stdout
+ * _puts - prints a string, followed by a newline to stdout
  *
  * @str: string parameter to print
  *
@@ -10,8 +10,7 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str + 0);
-		str++;
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
