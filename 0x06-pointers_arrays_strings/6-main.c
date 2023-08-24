15402193e35c9632fe14a0d2d9dda15469c0bb28
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-08-24 13:08:24 -0700

    j

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,11 +8,15 @@
  */
 int main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char str[1] = "Expect the best. Prepare for the worst. Capitalize";
+	char str[2] = "on what comes.\nhello world! hello-world";
+	char str[3] = "0123456hello world\thello world.hello world\n";
 	char *ptr;
 
 	ptr = cap_string(str);
 	printf("%s", ptr);
-	printf("%s", str);
+	printf("%s", str[1]);
+	printf("%s", str[2]);
+	printf("%s", str[3]);
 	return (0);
 }
---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[1] = "Expect the best. Prepare for the worst";
+	char s[2] = "Capitalize on what comes.\n";
+	char *p;
+
+	p = leet(s);
+	printf("%s", p);
+	printf("%s", s1);
+	printf("%s , s2);
+	return (0);
+}
