16f7215ac49e95fa300816115e6f531e291bcf0b
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-08-28 06:27:11 -0700

    rr

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - file memory with a constant type
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: pointer s
+ */
+
+char *_memset(char *s, char *b, unsigned int n)
+{
+	unsigned int test;
+
+	for (test = 0; n > 0; test++, n--)
+	{
+		s[test] = b;
+	}
+	return (s);
+}
