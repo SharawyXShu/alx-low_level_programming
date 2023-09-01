da1a7bbbfb4354f76e55a469c30ff387b0ddd6f9
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-09-01 08:44:52 -0700

    SS

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		sum = atoi(argv[1] * atoi(argv[2]);
-				printf("%d\n", sum);
-				}
-				else
-				{
-				printf("Error\n");
-				return (1);
-				}
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-				return (0);
+	return (0);
 }
