ee4a07172217ed9a788062b846b6c4eafb4b0e14
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-09-04 07:20:33 -0700

    ss

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -52,7 +52,7 @@ char **strtow(char *str)
 	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
-			for (j = 1; str[i + l] != ' ' && str[i + j]; j++)
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 				;
 			j++;
 			w[wc] = (char *)malloc(j * sizeof(char));
