f31d5f2a6e9a57724f8b3d76ac1586d9c1e3943e
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-09-07 07:09:23 -0700

    G

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,14 +30,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *m;
 
-	if (size == 0 || nmemb == 0)
+	if (size == 0 ||  nmemb == 0)
 		return (NULL);
 	m = malloc(sizeof(int) * nmemb);
 
 	if (m == 0)
 		return (NULL);
 
-	_memset(m, 0, sizeof(int) * nmemb);
+	*_memset(m, 0, sizeof(int) * nmemb);
 
 	return (m);
 }
