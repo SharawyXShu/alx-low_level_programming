af121c43a281277456d6643409d104aa46d78bb1
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-09-21 07:53:21 -0700

    S

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - linked list in singlee mode
+ * @str: string
+ * @len: string length
+ * @next: points to the next node
+ * Description: single linked list node structure
+ * for Holb Proj
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
