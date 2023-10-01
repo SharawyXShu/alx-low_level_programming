24c5217f546b7d6e5a80de7dfb426ccbfe684323
SharawyXShu <mohamed.sharawy1771996@gmail.com>
2023-10-01 13:26:54 -0700

    S

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,12 +14,12 @@ void print_binary(unsigned long int n)
 	{
 		if (n & 1L << --bit)
 		{
-			_putchar('1');
+			putchar('1');
 			printed++;
 		}
 		else if (printed)
-			_putchar('0');
+			putchar('0');
 	}
 	if (!printed)
-		_putchar('0');
+		putchar('0');
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <limits.h>
 
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 
 #endif
